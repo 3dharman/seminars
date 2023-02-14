fdc0ee0fb59d5bf7181b0d51131bb0b82088f6aa
dharman <featlord@yandex.ru>
2023-02-14 14:28:28 +0300

    added from -n to n console

---
+++ from_-n_to_n/Program.cs
@@ -0,0 +1,18 @@
+﻿// Printing a number from -N to N using the terminal //
+
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt32( Console.ReadLine());
+
+if (number > 0)
+    number *= -1; // number = number * (-1)
+
+int endNumber = number * -1;
+
+while (number <= endNumber) {
+    Console.Write(number);
+
+    if (number < endNumber)
+        Console.Write(", ");
+    
+    number++;
+}

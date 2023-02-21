b5768b0ab3d01a1bbd1cf8827e021617e89a5fcc
dharman <featlord@yandex.ru>
2023-02-21 20:04:23 +0300

    added squareing cosnole

--- squared/Program.cs
+++ squared/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Возведение числа в квадрат");
+﻿// Возведение числа в квадрат
+
+Console.WriteLine("Возведение числа в квадрат");
 Console.WriteLine("Пожалуйста, введите число и мы возведем его в квадрат");
 
 int number = Convert.ToInt32 (Console.ReadLine());

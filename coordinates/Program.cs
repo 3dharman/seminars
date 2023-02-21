046707f77538f0c698f9f79d4bf884202c6d504f
dharman <featlord@yandex.ru>
2023-02-21 20:02:25 +0300

    added coordinates console

---
+++ coordinates/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X не равно 0 и Y не равно 0 и выдаёт номер четверти плоскости,
+// в которой находится эта точка.
+
+int CalculateQuarter(int x, int y)
+{
+    if (x > 0 && y > 0)
+        return 1;
+    else if (x < 0 && y > 0)
+        return 2;
+    else if (x < 0 && y < 0)
+        return 3;
+    else if (x > 0 && y < 0)
+        return 4;
+    else 
+        return -1;
+}
+
+Console.WriteLine("Программа для вычисления номера четверти плоскости");
+Console.WriteLine("Введите координату Х: ");
+if (!int.TryParse(Console.ReadLine(), out var x))
+    Console.WriteLine("Введено не целое число");
+
+Console.WriteLine("Введите координату Y: ");
+if (!int.TryParse (Console.ReadLine(), out var y))
+    Console.WriteLine("Введено не целое число");
+
+if (x == 0 || y == 0) 
+{
+    Console.WriteLine("Точка не должна лежать на оси");
+    return;
+}
+var result = CalculateQuarter(x, y);
+if (result != -1)
+    Console.WriteLine($"Точка ({x}, {y}) находится в четверти {result}");
+else 
+    Console.WriteLine("Ошибка при вычислении, входные данные не верны");
